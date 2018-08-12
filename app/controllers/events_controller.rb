89c85e7bb76d70dd931a774bcc5c63eebf92be55
Manoj Naidu <manojnaidu619@gmail.com>
2018-08-12 11:03:43 +0530

    Added Image upload feature

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,6 +25,6 @@ class EventsController < ApplicationController
      private
 
      def event_params
-       params.require(:event).permit(:name, :description, :coordinator_id)
+       params.require(:event).permit(:name, :description, :coordinator_id, :image)
      end
 end
