549a2bbfa785c9f2a93c12cf61172960a3af5778
Manoj Naidu <manojnaidu619@gmail.com>
2018-08-12 11:14:20 +0530

    Image resized

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,6 +22,19 @@ class EventsController < ApplicationController
        @event = Event.find(params[:id])
      end
 
+     def edit
+       @event = Event.find(params[:id])
+     end
+
+     def update
+       @event = Event.find(params[:id])
+       if @event.update(event_params)
+         redirect_to event_path
+       else
+         render 'edit'
+       end
+     end
+
      private
 
      def event_params
