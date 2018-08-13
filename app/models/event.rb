1528bfe81c7cb41f8a16393e74b3ab62c0207284
Manoj Naidu <manojnaidu619@gmail.com>
2018-08-14 00:23:11 +0530

    Added Validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,11 @@
 class Event < ApplicationRecord
  belongs_to :coordinator        # Defining Association
+ validates :name, presence: true                         # name attribute validation
+ validates :description, presence: true            # description attribute validation
 
  has_one_attached :image        # Defining activestorage with a namespace
 
- def small_image
-   self.image.variant(resize: "300x300")
+ def small_image                                      # Method that returns the image
+   self.image.variant(resize: "300x300")           # Resizing the image and return it
  end
 end
