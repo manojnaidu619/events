89fc06922883a002dc8e9b2153aa30e4d2541de3
Manoj Naidu <manojnaidu619@gmail.com>
2018-08-13 21:55:32 +0530

    Meta tags missing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :coordinators
+  get '/coordinators/sign_up', to: redirect('/coordinators/sign_in')
   root 'home#index'
   resources :events, except: [:index]
   get '/dashboard', to: 'events#dashboard'
