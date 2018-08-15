31d817a4149f3e844457cdc1212374141e9e238a
Manoj Naidu <manojnaidu619@gmail.com>
2018-08-15 19:47:48 +0530

    Working PDF function using prawn

--- Gemfile
+++ Gemfile
@@ -22,7 +22,8 @@ gem 'coffee-rails', '~> 4.2'
 gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
-
+gem 'prawn'
+gem 'prawn-table'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,16 @@
 class HomeController < ApplicationController
   def index
     if coordinator_signed_in?
-      redirect_to dashboard_path              # Separate Root page to Logged in Co-ordinator!!
+      redirect_to dashboard_path                                                  # Separate Root page to Logged in Co-ordinator!!
     end
     @events = Event.all
+     respond_to do |format|
+       format.html
+       format.pdf do                                                              # Responding to .pdf format
+         pdf = Prawn::Document.new                                                # Create new Prawn Object
+         pdf.text "It's Working"                                                  # Appending Text
+         send_data pdf.render, type: 'application/pdf', disposition: 'inline'     # Outputting the pdf file, disposition means showing the file inside browser
+       end
+     end
   end
 end
